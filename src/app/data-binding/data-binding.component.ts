426612a16a18cf8b224ee3f1e01512e9c2f52c3a
marciocostarodrigues <marciodeath@hotmail.com>
2022-08-21 10:37:50 -0300

    output property

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -18,6 +18,8 @@ export class DataBindingComponent implements OnInit {
 
   nomeDoCurso: string = 'Angular';
 
+  valorInicial: number = 15;
+
   getValor() {
     return 1;
   };
@@ -44,6 +46,10 @@ export class DataBindingComponent implements OnInit {
 
   onMouseOverOut() {
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento) {
+    console.log(evento.novoValor)
   }
 
   constructor() { }
---
+++ src/app/output-property/output-property.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './output-property.component.html',
+  styleUrls: ['./output-property.component.scss']
+})
+export class OutputPropertyComponent implements OnInit {
+
+  @Input() valor: number = 0;
+
+  @Output() mudouValor = new EventEmitter();
+
+  incrementa() {
+    this.valor++;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
+
+  decrementa() {
+    this.valor--;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
+
+  constructor() {  }
+
+  ngOnInit() {
+  }
+
+}
