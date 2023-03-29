9c677308d4dbd4de624ff49323d826fdbcb0c7bf
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-03-28 22:55:49 -0300

    pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common'
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) => settingsService.getLocale()
+    },
+    // {
+    //   provide: DEFAULT_CURRENCY_CODE,
+    //   useValue: 'BRL'
+    // }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
