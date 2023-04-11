24d37125b2b44488300f2b917c43e6fde8af647a
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-04-10 22:19:18 -0300

    rotas

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CursosService } from '../cursos/cursos.service';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.scss']
+})
+export class CursoDetalheComponent implements OnInit, OnDestroy {
+
+  id: number;
+  subscription: Subscription;
+  curso: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cursosService: CursosService
+  ) { 
+    //this.id = this.route.snapshot.params['id'];
+  }
+  
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+
+        this.curso = this.cursosService.getCurso(this.id);
+
+        if (this.curso == null) {
+          this.router.navigate(['/naoEncontrado']);
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
