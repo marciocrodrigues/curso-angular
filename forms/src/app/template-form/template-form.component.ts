6db69374ed4e3d926c682360abd73206166e3075
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-05-08 23:21:13 -0300

    Novos campos no formulario

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -9,7 +9,14 @@ export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
     nome: null,
-    email: null
+    email: null,
+    cep: null,
+    numero: null,
+    complemento: null,
+    rua: null,
+    bairro: null,
+    cidade: null,
+    estado: null
   }
 
   constructor() { }
